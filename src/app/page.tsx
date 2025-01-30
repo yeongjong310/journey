90ded2429d30ede4d1c67b8279196342530f1d61
tory-inflab <yeongjongkim@inflab.com>
2025-01-31 00:41:58 +0900

    feat: home 소개 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Tory&apos;s Journey
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        면목 모동네에서 태어났다. 그 어린아이의 집은 만화방을 했다. 그의
+        어머니는 만화를 보고 돈을 안내는 조직 폭력배들에게 찬물을 끼얹었다.
+        (강직하셨다는거야 어머님이) 그 동네에서는 처음으로 22인치 흑백 TV가
+        있었다. 그때부터 TV와 너무 친해지게 되었다. 엎어지고 넘어지는 슬랩 스틱
+        코미디를 처음 맛본 4살 먹은...
       </p>
       <div className="my-8">
         <BlogPosts />
