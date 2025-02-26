aac5b67f3d331b6f6a096f010834adc57b51f4f4
토리 <yeongjongkim@inflab.com>
2025-02-26 20:41:12 +0900

    fix: r3f 타입 깨지는 이슈 임시 주정

--- src/app/components/r3f/Canvas.tsx
+++ src/app/components/r3f/Canvas.tsx
@@ -1,12 +1,14 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
 "use client";
 
 import MyElement3D from "./MyElement3D";
 import dynamic from "next/dynamic";
-
-const FiberCanvas = dynamic(
-  () => import("@react-three/fiber").then((mod) => mod.Canvas),
-  { ssr: false }
-);
+const FiberCanvas = dynamic<
+  // @ts-expect-error
+  React.ComponentProps<typeof import("@react-three/fiber").Canvas>
+  // @ts-expect-error
+>(() => import("@react-three/fiber").then((mod) => mod.Canvas), { ssr: false });
 
 export function Canvas() {
   return (
